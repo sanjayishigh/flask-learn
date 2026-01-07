a9539c153790b746bfa58c9b17ec18ba8a160e2d
sanjayishigh <sanjayishigh@gmail.com>
2026-01-07 11:51:18 +0530

    basic form

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    mylist = [1,2,3,4,5]
-    return render_template('index.html', myvalue=[1,2,3,4], mylist=mylist)
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == 'Sanjay' and password== '123':
+            return "Success"
+        else:
+            return "failure"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
